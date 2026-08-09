24ac519695d513aed942bbc7dbf304f85820279e
Akash bisht <aayush45r@gmail.com>
2026-08-09 12:31:05 +0530

    fixed the why join us section in mobile
    Signed-off-by: Akash bisht <aayush45r@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  allowedDevOrigins: ["monsoon-suspend-uncloak.ngrok-free.dev"],
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
